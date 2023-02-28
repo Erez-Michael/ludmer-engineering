5e3094053910b42f681ef4e9d15a658a32c8159f
erezmichael <erezmichael01@gmail.com>
2023-02-27 21:18:34 -0500

    test3

--- src/data/FeaturesData.js
+++ src/data/FeaturesData.js
@@ -9,41 +9,38 @@ const iconStyle = (Icon) => <Icon size="3rem" color="#0f0f0f" />;
 
 export const featuresData = [
   {
-    name: "Best Security",
-    description:
-      "We offer the best data security to our clients, which makes us stand out",
+    name: "Lorem",
+    description: "Our team is available at all times in case you need us",
     icon: iconStyle(BsFillShieldLockFill),
     imgClass: "one",
   },
   {
-    name: "Ease of Use",
-    description: "Our system is easy to use and integrate",
+    name: "Lorem",
+    description: "Our team is available at all times in case you need us",
     icon: iconStyle(IoIosOptions),
     imgClass: "two",
   },
   {
-    name: "Maintenance",
-    description:
-      "Our code is written in highest standards, which makes it highly sustainable",
+    name: "Lorem",
+    description: "Our team is available at all times in case you need us",
     icon: iconStyle(GrHostMaintenance),
     imgClass: "three",
   },
   {
-    name: "24/7 Support",
+    name: "Lorem",
     description: "Our team is available at all times in case you need us",
     icon: iconStyle(BiSupport),
     imgClass: "four",
   },
   {
-    name: "Price",
-    description: "We offer the highest value/cost ratio",
+    name: "Lorem",
+    description: "Our team is available at all times in case you need us",
     icon: iconStyle(BiDollar),
     imgClass: "five",
   },
   {
-    name: "Scalable",
-    description:
-      "Our servers are located all over the world, therefore improving scalability and speed ",
+    name: "Lorem",
+    description: "Our team is available at all times in case you need us",
     icon: iconStyle(AiOutlineCloudUpload),
     imgClass: "six",
   },
